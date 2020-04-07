8703d4b838f66ff21e099bee7c8013a15ea36356
Ashleigh Christopher <ashleighc207@gmail.com>
2020-04-07 10:27:51 -0400

    enable watch for css files

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: "./client/index.js",
+  mode: "development",
+  watch: true,
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "bundle.js"
@@ -24,22 +26,16 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, "/public"), // serve your static files from here
-    watchContentBase: true, // initiate a page refresh if static content changes
+    contentBase: path.join(__dirname, "/dist"),
+    watchContentBase: true,
     proxy: [
-      // allows redirect of requests to webpack-dev-server to another destination
       {
-        context: ["/api", "/auth"], // can have multiple
-        target: "http://localhost:4000", // server and port to redirect to
+        context: ["/api"],
+        target: "http://localhost:4000",
         secure: false
       }
     ],
-    port: 8080, // port webpack-dev-server listens to, defaults to 8080
-    overlay: {
-      // Shows a full-screen overlay in the browser when there are compiler errors or warnings
-      warnings: false, // defaults to false
-      errors: false // defaults to false
-    }
+    port: 8080
   },
   plugins: [new HtmlWebpackPlugin({ template: "./client/index.html" })]
 };
